aafc6ca34f1d4b2619c896027f83dcea17078574
Adam Carr <addycarrr@gmail.com>
2023-09-21 12:47:22 +0200

    initialisation

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+    <div class="navbar">
+      <div class="navbar-logo">
+        {/* <img src="logo.png" alt="Logo"> */}
+      </div>
+      <div class="navbar-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+    
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Crypto Decrypted</h1>
+        <h4>A latecomer's guide to crypto</h4>
       </header>
+      <div class="wrapper">
+        <h2>Intro</h2>
+        <p>There exists more uncertainty in the world right now than other time in the last 50 years. Progression of AI has shook the average person (before being adopted without a blink within a year), geopolitics shakiness looms over world events, inflation hit unseen levels two years ago as we had a major worldwide pandemic all as we try to ignore just how good the big data algorithms are at knowing us and our actions.
+          </p>
+          <p>
+Yet despite it's volatility, over a 2 year moving average the crypto market cap has not seized to keep its momentum in the face of all of this.</p>
+
+<p>For each of the worldwide issues mentioned, many see blockchain and crypto currency as a shining hope in the future digital landscape, and it's time to understand why.</p>
+<h2>Intro</h2>
+<h2>Intro</h2>
+<h2>Intro</h2>
+      </div>
     </div>
   );
 }
